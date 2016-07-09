248139d01936b3ac807d39cf87aadfa6620cc0f1
askarisback@gmail.com <askarisback@gmail.com>
2016-07-09 10:40:38 +0500

    favicon-resolver

--- web.js
+++ web.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var favicon = require('serve-favicon');
 var port = process.env.PORT || 8080;
+
+app.use(favicon('./public/favicon.ico'));
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
