4fcdefbcbce02b0145fbd3753e89704558485c98
1mp0ss1ble <askarisback@gmail.com>
2016-07-11 13:43:35 +0500

    Update web.js

--- web.js
+++ web.js
@@ -12,6 +12,17 @@ app.get('/', function (req, res) {
   res.send('Hello Ichi!');
 });
 
+
+app.all('/user/:id/:op?', function(req, res, next) {
+    req.user = users[req.params.id];
+    if (req.user) {
+        next();
+    } else {
+        next(new Error('cannot find user ' + req.params.id));
+    }
+});
+
+
 app.get('/user/:id', function(req, res) {
     res.send('viewing ' + req.user.name);
 });
