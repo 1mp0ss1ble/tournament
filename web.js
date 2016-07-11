4dd81328e4f620da42716fc9a3bf96293833e002
1mp0ss1ble <askarisback@gmail.com>
2016-07-11 13:41:33 +0500

    Update web.js

--- web.js
+++ web.js
@@ -2,10 +2,18 @@ var express = require('express');
 var app = express(); 
 //var favicon = require('serve-favicon');
 var port = process.env.PORT || 3000;
+var users = [{
+    name: 'Nurzhinho'
+}];
+
 
 //app.use(favicon('./public/favicon.ico'));
 app.get('/', function (req, res) {
   res.send('Hello Ichi!');
+});
+
+app.get('/user/:id', function(req, res) {
+    res.send('viewing ' + req.user.name);
 });
 
 
