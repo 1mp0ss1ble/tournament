becfad176f7cf24502e6e844016204bb5aa0c03a
askarisback@gmail.com <askarisback@gmail.com>
2016-10-31 23:39:43 +0500

    favicon fix

--- web.js
+++ web.js
@@ -55,7 +55,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'jade');
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 
 app.use(require('./controllers'))
